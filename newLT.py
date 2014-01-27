85c618d199ff1c03dad7a13103bc2bf76c040f72
nbliss <nbliss2@uic.edu>
2014-01-27 11:12:56 -0600

    Added newLT for finding weighted leading terms

---
+++ newLT.py
@@ -0,0 +1,73 @@
+class monomialOrdering(object):
+	"""
+	Object to compute the leading term based on a monomial ordering. 
+	Sympy already has the option of computing leading term based on
+	lex, grlex, grevlex, and elimination order, so if you're going to
+	use one of those orderings just stick with sympy's, as it's much
+	more efficient (monomials are ordered at time of polynomial creation.
+	I've added support for lex, grlex, and grevlex for convenience, but
+	the primary purpose of this class is for doing weighted ordering.
+	"""
+	from sympy import Poly
+	def __init__(self,variables,ordering='lex',weightVector=(1,1,1)):
+		"""
+		Checks that the ordering is one of the ones we can deal with.
+		Requires the sympy variables to be passed through (as a list)
+		so LT can return an actual sympy polynomial (which needs the
+		variables in its initialization)
+		"""
+		assert (ordering in ['lex','grlex','grevlex','weighted'])
+		self.ordering = ordering
+		self.nv = len(variables)
+		self.wv = weightVector
+
+	def LT(poly):
+		polyDict = poly.as_dict()
+		exponents = polyDict.keys()
+		largest = exponents[0]
+		for i in xrange(len(polyDict)):
+			if compare(exponents[i],largest): largest = exponents[i]
+		return Poly({largest:polyDict[largest]},self.variables)
+
+	def compare(self,monA,monB,lex=False):
+		"""
+		Based on monomial ordering, returns True if first is greater, False otherwise
+		"""
+		
+		if self.ordering=='lex' or lex==True:
+			for i in xrange(self.nv):
+				if monA[i]>monB[i]:
+					return True
+				elif monA[i]<monB[i]:
+					return False
+				else: continue
+			return True
+		elif self.ordering=='grlex':
+			sumA,sumB = sum(monA),sum(monB)
+			if sumA==sumB:
+				return self.compare(monA,monB,lex=True)
+			elif sumA>sumB: return True
+			else: return False
+		elif self.ordering=='grevlex':
+			sumA,sumB = sum(monA),sum(monB)
+			if sumA==sumB:
+				for i in xrange(self.nv):
+					if monA[self.nv-i-1]>monB[self.nv-i-1]:
+						return False
+					elif monA[self.nv-i-1]<monB[self.nv-i-1]:
+						return True
+					else: continue
+				return True
+			elif sumA>sumB: return True
+			else: return False
+		else:
+			assert self.ordering=='weighted'
+			dotA = sum(monA[i]*self.wv[i] for i in xrange(self.nv)) 
+			dotB = sum(monB[i]*self.wv[i] for i in xrange(self.nv)) 
+			if dotA>dotB: return True
+			elif dotA<dotB: return False
+			else: return self.compar(monA,monB,lex=True)
+
+if __name__=="__main__":
+	mono = monomialOrdering(3,ordering='weighted',weightVector=(1,3,4))
+	print mono.compare((4,2,4),(3,3,3)) 
