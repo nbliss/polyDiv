0a4a44d2173f517267c8a17dd6c87931ff12dc97
sommars <sommars1@uic.edu>
2014-01-29 19:29:31 -0600

    Update groeb.py

--- groeb.py
+++ groeb.py
@@ -8,39 +8,34 @@ s = [x,y]
 
 
 def groebasis(f,s):
-	print f
 	from newLT import *
-        x,y,z = symbols('x,y,z')
-        symbolList = [x,y,z]
-        mono = monomialOrdering(symbolList,ordering='lex')
+        mono = monomialOrdering(s,ordering='lex')
 	g = f
 	h = 0
 	while(g != h):
-	 h = list(g)
-	 for S in subsets(f,2):
-		if S[0] != S[1]:
-			print "Yo there will be an error below the second time you see this message"
-			print S
-			print ""
-			Spoly = (Poly(lcm(mono.LM(S[0]),mono.LM(S[1]))/mono.LT(S[0]))*S[0] - Poly(lcm(mono.LM(S[0]),mono.LM(S[1]))/mono.LT(S[1]))*S[1]).expand()
-			print Spoly
-
-			r = Spoly
-			k = 0
-		 #From here to the next "#" I just threw in the code from lec4
-		 	while(r!=0 and k!= -1):
-		        	k = -1
-		        	for i in range(len(f)):
-		         #If there's no common root(x+y, x**2 - 1, y**2 - 2*x), then it dies at the line below. I think what it is that kills it is that it gets an r value which is an integer. If r is an int, does that always mean that it's going to have a common root?
-		         		if div(mono.LT(r),mono.LT(f[i]))[1] == 0:
-		               			k = i
-		                		break
-		        	if k!=-1:
-		        		r = (r - div(mono.LT(r),mono.LT(f[i]))[0]*f[k]).expand()
-		 #
-
-		if r!= 0:
-			g.append(Spoly)
+		h = list(g)
+		for S in subsets(f,2):
+			if S[0] != S[1]:
+				AA = lcm(mono.LM(S[0]),mono.LM(S[1]))/mono.LT(S[0])
+				BB = lcm(mono.LM(S[0]),mono.LM(S[1]))/mono.LT(S[1])
+				if type(AA) != Poly:
+					AA = Poly(AA,s)
+				if type(BB) != Poly:
+					BB = Poly(BB,s)
+				Spoly = AA*S[0] - BB*S[1]
+				r = Spoly
+				k = 0
+			 	while(r!=0 and k!= -1):
+			        	k = -1
+			        	for i in range(len(f)):
+			         #If there's no common root(x+y, x**2 - 1, y**2 - 2*x), then it dies at the line below. I think what it is that kills it is that it gets an r value which is an integer. If r is an int, does that always mean that it's going to have a common root?
+			         		if div(mono.LT(r),mono.LT(f[i]))[1] == 0:
+			               			k = i
+			                		break
+			        	if k!=-1:
+			        		r = (r - div(mono.LT(r),mono.LT(f[i]))[0]*f[k]).expand()
+			if r!= 0:
+				g.append(Spoly)
 	print "Groebner basis below:"
 	print g
 
