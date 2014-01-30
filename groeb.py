8e7fef390347d22168029e22207aa9f988b41f81
sommars <sommars1@uic.edu>
2014-01-30 08:11:58 -0600

    Update groeb.py

--- groeb.py
+++ groeb.py
@@ -5,7 +5,7 @@ y = symbols('y')
 z = symbols('z')
 f = [Poly(x**2+1),Poly(x*y-1)]
 s = [x,y]
-
+f = [Poly(3*x**2+1,s),Poly(x*y-1,s)]
 
 def groebasis(f,s):
 	from newLT import *
@@ -39,7 +39,35 @@ def groebasis(f,s):
 	print "Groebner basis below:"
 	print g
 
+	#http://www.kent.ac.uk/smsas/personal/gdb/MA574/week6.pdf
+	#The above algorithm is implemented below. First, getting a minimal groebner basis:
+
+
+	#The lines below are intended to get the groebner basis monic
+	for k in g:
+		#The line below should give the leading coefficient of a polynomial. Currently, it just returns None. I think this means you need to add a new coeff function?
+		if(k.coeff(mono.LM(k)) != 1):
+			g[g.index(k)] = k/(k.coeff(mono.LM(k)))
+	print g
+
+
+
+	#Once monic, proceed with minimal
+
+	H2 = [] #Using notation of kent algorithm
+	minimalg = []
+	for k in g:
+		if mono.LT(k) not in H2:
+			H2.append(mono.LT(k))
+			minimalg.append(k)
+
+	#The above shoud should create the final minimal groebnerbasis 
+	
+	#I guess I'm mentally struggling to implement the reduction part of the algorithm. Again, I don't think it's bad, I'm probably just being dumb. I think we should be easily able to throw that part together tomorrow afternoon.
+
+
 	return
+
 
 #So the above is a direct implementation of the algorithm. It works. Below, I've tried to add a way to get the reduced groebner basis. It might work..? It works sometimes, that's for sure.
 if __name__=="__main__":
